6b0c78d5c2290a3964d2038ab792b611d610f0e9
Alejandro Hernández <Alejandrehl@icloud.com>
2019-11-05 18:02:31 -0500

    Add fields to model

--- models/Article.js
+++ models/Article.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose");
 
 const ArticleSchema = mongoose.Schema({
-    title: {type: String},
-    story_title: {type: String},
-    created_at: {type: Date}
+    created_at: Date,
+    title: String,
+    url: String,
+    author: String,
+    points: Number,
+    story_text: String,
+    comment_text: String,
+    num_comments: Number,
+    story_id: Number,
+    story_title: String,
+    story_url: String,
+    parent_id: Number,
+    created_at_i: Number,
+    objectID: String,
 });
 
 module.exports = mongoose.model("Article", ArticleSchema);
