41d42b878224c2fd241cea292b7d48bbe46393b9
Alejandro Hernández <Alejandrehl@icloud.com>
2019-11-05 16:28:28 -0500

    Home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import Home from "./components/pages/Home";
 
 const App = () => {
     return (
-        <div>
-            <h1>Welcome</h1>
-        </div>
+        <Home />
     );
 };
 
