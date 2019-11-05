461d08272228f462201758a800dcecb4a22a6ff5
Alejandro Hernández <Alejandrehl@icloud.com>
2019-11-05 18:10:00 -0500

    Multiple docs inserted to collection

--- utils/Algolia.js
+++ utils/Algolia.js
@@ -1,10 +1,16 @@
 const request = require("request");
+const Article = require("../models/Article");
 
 const getData = () => {
     request("https://hn.algolia.com/api/v1/search_by_date?query=nodejs", (error, response, body) => {
         if (error) return console.log("Error:", error);
         const articles = JSON.parse(body);
         console.log("Número de objetos:", articles.hits.length);
+        Article.collection.insert(articles.hits, (error, docs) => {
+            if (error) return console.log(error);
+            console.log("Multiple docs inserted to collection.");
+            console.log(docs);
+        })
     });
 };
 
