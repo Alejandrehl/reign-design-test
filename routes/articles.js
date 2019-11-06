4ccf45331f1e1bd43fde8d715c2204a596c2646c
Alejandro Hernández <Alejandrehl@icloud.com>
2019-11-05 21:19:44 -0500

    Api to get articles

--- routes/articles.js
+++ routes/articles.js
@@ -2,4 +2,16 @@ const express = require("express");
 const router = express.Router();
 const Article = require("../models/Article");
 
+// @route   GET api/articles
+// @desc    Get all users articles
+// @access  Public
+router.get('/', async (req, res) => {
+    try {
+        const articles = await Article.find().sort({created_at: -1});
+        res.json(articles)
+    } catch (err) {
+        res.status(500).send('Server Error')
+    }
+});
+
 module.exports = router;
