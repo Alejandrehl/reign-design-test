0fe9bb5fe8f25c3bda2cc1c7d1e3a4ca1590c126
Alejandro Hernández <Alejandrehl@icloud.com>
2019-11-05 21:02:41 -0500

    Fix cron

--- utils/Algolia.js
+++ utils/Algolia.js
@@ -3,19 +3,24 @@ const CronJob = require('cron').CronJob;
 const Article = require("../models/Article");
 
 const getData = () => {
-    new CronJob('0 0 */1 * * *', function() {
+    new CronJob('0 0 * * * *', function() {
         console.log('You will see this message every hour');
 
         request("https://hn.algolia.com/api/v1/search_by_date?query=nodejs", (error, response, body) => {
             if (error) return console.log("Error:", error);
+
             const articles = JSON.parse(body);
+
             Article.collection.insertMany(articles.hits, (error, docs) => {
                 if (error) console.log(error.err.errmsg);
-                console.log("Multiple docs inserted to collection.");
+                const d = new Date();
+                console.log("DOCS:", docs);
+                console.log("Multiple docs inserted to collection: ", d);
             })
+
         });
 
-    }, null, true, 'America/Los_Angeles');
+    }, null, true, 'America/New_York');
 };
 
 module.exports = getData;
