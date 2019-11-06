39ee5cec13df2766de324fd010c7a54b57c7d186
Alejandro Hernández <Alejandrehl@icloud.com>
2019-11-06 00:22:25 -0500

    First part of frontend

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,7 +1,11 @@
 import React, {useState, useEffect} from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from '@material-ui/core/Typography';
 import api from "../../utils/Api";
 
 const Home = () => {
+    const classes = useStyles();
     const [articles, setArticles] = useState(null);
 
     useEffect(() => {
@@ -21,10 +25,28 @@ const Home = () => {
 
     return (
         <div>
-            <h1>Home Page</h1>
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Typography variant="h1" className={classes.paperText}>
+                    HN Feed
+                </Typography>
+                <Typography variant="h5" className={classes.paperText}>
+                    {"We <3 hacker news!"}
+                </Typography>
+            </div>
             {renderArticles()}
         </div>
     );
 };
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        backgroundColor: "black",
+        padding: theme.spacing(7, 4),
+    },
+    paperText: {
+        color: "white"
+    }
+}));
 
 export default Home;
